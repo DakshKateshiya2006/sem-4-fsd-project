68a634d38f0615d1834ed52038a8d5b14bc4ec07
DakshKateshiya2006 <10sepon2006@gmail.com>
2026-06-29 19:04:37 +0530

    cors setup completed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,13 +1,29 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
+const Cors = require("cors");
 const app = express();
+
+// Default Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+app.use(Cors({
+    origin: [process.env.DEV_FRONTEND_ORIGIN, process.env.PRODUCTION_FRONTEND_ORIGIN],
+    secure: process.env.PRODUCT === "production",
+    credentials: true
+}))
+
 // Health Route
 app.get("/health", (req, res) => {
-  res.status(200).json({
-    status: "OK",
-    message: "Server is healthy",
-    uptime: process.uptime(),
-    timestamp: new Date().toISOString(),
-  });
+    res.status(200).json({
+        status: "OK",
+        message: "Server is healthy",
+        uptime: process.uptime(),
+        timestamp: new Date().toISOString()
+    });
 });
+
+// All Routes.
+
 
 module.exports = app;
